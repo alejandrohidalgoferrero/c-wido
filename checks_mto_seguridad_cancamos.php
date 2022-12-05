e0dd6e6b45d5ef3d6ce272f2635a70157cbf1cd7
HIDALGO-FERRERO Alejandro <alejandro.hidalgo-ferrero-extern@renault.com>
2022-12-05 13:36:37 +0100

    Checks seguridad mto 4 carga

--- checks_mto_seguridad_cancamos.php
+++ checks_mto_seguridad_cancamos.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-     <link type="text/css" href="http://localhost/D-WIDO/css/bootstrap.min.css" rel="stylesheet">
+     <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet">
      <link rel="stylesheet" href="js/icofont/icofont.min.css">
      <script src="js/jquery.min.js"></script>
 
